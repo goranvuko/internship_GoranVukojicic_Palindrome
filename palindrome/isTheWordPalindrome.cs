2aa4cced05ec28414e499548f42702e8d1e69d62
Goran Vukojičić <goranvukojicic@gmail.com>
2022-07-04 17:14:03 +0200

    Add project files.

---
+++ palindrome/isTheWordPalindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    public class isTheWordPalindrome
+    {
+        public static Boolean IsPalindrome()
+        {
+            string str = "";
+            Console.WriteLine("Enter string:");
+
+            str = Console.ReadLine();
+            str = str.Replace(" ", "");
+
+            str = str.Replace("'", "");
+            str = str.Replace(".", "");
+            str = str.Replace(",", "");
+            str = str.Replace("!", "");
+            str = str.Replace("?", "");
+            str = str.ToLower();
+
+            for (int i=0; i<str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length -1 - i]) return false;
+            }
+            return true;
+        }
+    }
+}
